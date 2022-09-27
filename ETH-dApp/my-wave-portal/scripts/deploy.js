d943e171c2938c4e00d32b3da97d61f4c355b6ce
kazumal <kazuma.hamamoto12@gmail.com>
2022-09-27 15:12:50 +0900

    add original function

---
+++ ETH-dApp/my-wave-portal/scripts/deploy.js
@@ -0,0 +1,22 @@
+const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy({
+    value: hre.ethers.utils.parseEther("0.1"),
+  });
+  const wavePortal = await waveContract.deployed();
+
+  console.log("Contract deployed to:", wavePortal.address);
+  console.log("Contract deployed by:", owner.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+runMain();
